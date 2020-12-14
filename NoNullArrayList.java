edc907975eb4f4b57bc355256f59cb6aba1e52c2
emmy-chen <>
2020-12-14 11:34:06 -0500

    overrode first add method

--- NoNullArrayList.java
+++ NoNullArrayList.java
@@ -1,7 +1,32 @@
 import java.util.ArrayList;
-NoNullArrayList<T> extends ArrayList<T>;
-public class NoNullArrayList {
-  private ArrayList<String> default;
+public class NoNullArrayList<T> extends ArrayList<T> {
+  private ArrayList<T> default;
   private int startingCapacity;
-  
+
+  public NoNullArrayList() {
+    super();
+  }
+
+  public NoNullArrayList(int startingCapacity) {
+    super(startingCapacity);
+  }
+
+  /*
+
+  public boolean add(String elment) {
+    if (element == null) {
+      throw new IllegalArgumentException("a null value cannot be added to a NoNullArrayList");
+    }
+  }
+
+  public void add(int index) {
+
+  }
+
+  public String set(int index, String value) {
+
+  }
+
+  */
+
 }
